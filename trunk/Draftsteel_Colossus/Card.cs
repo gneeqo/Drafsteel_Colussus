20e5ddafb0726cb923fc3c10b665a70cb75689a3
nico.toe <nico.toe@f9634755-8fcf-4b3d-a444-0711b88f0293>
2023-11-17 04:41:30 +0000

    skeleton for card class

--- trunk/Draftsteel_Colossus/Card.cs
+++ trunk/Draftsteel_Colossus/Card.cs
@@ -8,5 +8,52 @@ namespace Draftsteel_Colossus
 {
     class Card
     {
+
+        String Name;
+        enum cardType
+        { 
+            creature,
+            land,
+            instant,
+            sorcery,
+            enchantment,
+            artifact,
+            planeswalker,
+            battle
+        };
+        
+
+
+        float white;
+        float black;
+        float blue;
+        float red;
+        float green;
+
+        cardType type;
+
+        float value;
+
+        float infamy;
+
+        float aggro;
+        float reanimate;
+        float breach;
+        float ramp;
+        float control;
+        float artifacts;
+        float storm;
+        float sacrifice;
+        float blink;
+        float lands;
+
+        List<String> combos_names;
+        List<String> nonbos_names;
+
+        List<Card> combos;
+        List<Card> nonbos;
+
+
+
     }
 }
